9eb92cd1e48777f90075af1c13bbc3ca5305027e
unknown <nwosuemmanuel159@gmail.com>
2026-02-18 08:10:23 +0100

    last

--- reallysimplesocial/settings.py
+++ reallysimplesocial/settings.py
@@ -109,6 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = 'users.User'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
